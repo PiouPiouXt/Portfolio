27f53f93599d4b1e6103abf2a2757a9a9dff0a2a
Piou <tsiresyrazak19@gmail.com>
2026-07-24 21:27:34 +0300

    finishing Testimonials Section

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,4 +1,35 @@
+import { Mail, Phone, MapPin } from 'lucide-react';
+
+const contactInfo = [
+  {
+    icon: Mail,
+    label: "Email",
+    value: "tsiresyrazak26@gmail.com",
+    href: "mailto:tsiresyrazak26@gmail.com"
+  },
+  {
+    icon: Phone,
+    label: "Phone",
+    value: "+261389899778",
+    href: "tel:+261389899779"
+  },
+  {
+    icon: MapPin,
+    label: "Location",
+    value: "Antananarivo",
+    href: "#"
+  },
+
+]
+
+
 export const Contact = () => {
-  return <section></section>
-}
+  return (
+    <section
+    id="testimonials"
+    className="py-32 relative overflow-hidden">
+
+    </section>
+  );
+};
 
--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,22 +1,43 @@
-import { Quote } from 'lucide-react';
+import { Quote, ChevronLeft, ChevronRight } from 'lucide-react';
+import { useState } from 'react';
 
 
 const testimonials =
-[
-  {
-    quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni inventore ratione, minima laboriosam",
-    author: "ta mere",
-    role: "hacker",
-    avatar:"/public/profile-picture.jpg"
-  },
-] 
+  [
+    {
+      quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni inventore ratione, minima laboriosam",
+      author: "ta mere",
+      role: "hacker",
+      avatar: "/profile-picture.jpg"
+    },
+    {
+      quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni inventore ratione, minima laboriosam",
+      author: "Miouuuu",
+      role: "Ta reuf",
+      avatar: "/profile-picture.jpg"
+    },
+    {
+      quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni inventore ratione, minima laboriosam",
+      author: "ton pere",
+      role: "Piou",
+      avatar: "/profile-picture.jpg"
+    },
+  ]
 
 export const Testimonials = () => {
+  const [activeIdx, setActiveIdx] = useState(0);
+
+  const next = () => {
+    setActiveIdx((prev) => (prev + 1) % testimonials.length);
+  }
+  const previous = () => {
+    setActiveIdx((prev) => (prev - 1 + testimonials.length) % testimonials.length);
+  }
   return (
     <section
       id="testimonials"
       className="py-32 relative overflow-hidden"
-    > 
+    >
       <div
         className="absolute top-1/2 left-1/2 w-[800px] h-[800px] bg-primary/5 rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2"
       />
@@ -52,30 +73,58 @@ export const Testimonials = () => {
         <div className='max-w-4xl mx-auto'>
           <div className='relative'>
             {/* Main Testimonial */}
-            <div className='glass p-8 rounded-3xl md:p-12 glow-border animate-fade-in animate-delay-200'> 
+            <div className='glass p-8 rounded-3xl md:p-12 glow-border animate-fade-in animate-delay-200'>
               <div className='absolute -top-4 left-9 w-12 h-12 rounded-full bg-primary flex items-center justify-center'>
-                <Quote className='2-6 h-6 text-primary-foreground'/>
+                <Quote className='2-6 h-6 text-primary-foreground' />
               </div>
 
               <blockquote className='text-xl md:text-2xl font-medium leading-relaxed mb-8 pt-4'>
-                "{testimonials[0].quote}"
+                "{testimonials[activeIdx].quote}"
               </blockquote>
 
               <div className='flex items-center gap-4'>
                 <img
-                  src={testimonials[0].avatar} 
-                  alt={testimonials[0].author}
+                  src={testimonials[activeIdx].avatar}
+                  alt={testimonials[activeIdx].author}
                   className='w-14 h-14 rounded-full object-cover ring-2 ring-primary/20'
                 />
                 <div className='flex items-center gap-4'>
                   <div className='font-semibold'>
-                    {testimonials[0].author}
+                    {testimonials[activeIdx].author}
                   </div>
                   <div className='text-sm text-muted-foreground'>
-                    {testimonials[0].role}
+                    {testimonials[activeIdx].role}
                   </div>
                 </div>
               </div>
+            </div>
+
+            {/* Testimonials Navigation */}
+            <div className='flex items-center justify-center gap-4 mt-8'>
+              <button className='p-3 rounded-full glass hover:bg-primary/10 hover:text-primary transition-all'
+                onClick={previous}
+              >
+                <ChevronLeft />
+              </button>
+
+              <div className='flex gap-2'>
+                {testimonials.map((_, idx) =>
+                (
+                  <button
+                    onClick={() => setActiveIdx(idx)}
+                    className={`w-2 h-2 rounded-full transition-all duration-300 
+                    ${idx === activeIdx
+                        ? "w-8 bg-primary"
+                        : "bg-muted-foreground/30 hover:bg-muted-foreground/50"}`} />
+                ))}
+              </div>
+
+              <button className='p-3 rounded-full glass hover:bg-primary/10 hover:text-primary transition-all'
+                onClick={next}
+              >
+                <ChevronRight />
+              </button>
+
             </div>
           </div>
         </div>
